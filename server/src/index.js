ae05915c9d1126d15b3bd0e323d8ecee1bccfaa7
tiyawt <tiyaland3@gmail.com>
2025-10-12 20:35:26 +0700

    add swagger doc

--- server/src/index.js
+++ server/src/index.js
@@ -1,22 +1,38 @@
 import express from "express";
 import dotenv from "dotenv";
+import fs from "fs";
+import yaml from "yaml";
+import swaggerUi from "swagger-ui-express";
+import path from "path";
+import { fileURLToPath } from "url";
+
 import { connectDB } from "./config/db.js";
 import authRoutes from "./routes/auth.routes.js";
 import todoRoutes from "./routes/todo.routes.js";
 
+
 dotenv.config();
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const specPath = path.join(__dirname, "../openapi.yaml");
+const spec = yaml.parse(fs.readFileSync(specPath, "utf8"));
+
+
 const app = express();
 app.use(express.json());
+app.use("/docs", swaggerUi.serve, swaggerUi.setup(spec, { explorer: true }));
 
+// DB connect
 await connectDB();
 
 app.get("/", (req, res) => res.send("🚀 Server is running!"));
-
-// Routes
 app.use("/api/auth", authRoutes);
-app.use("/api/todos", todoRoutes); //butuh auth
+app.use("/api/todos", todoRoutes);
 
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, () =>
-  console.log(`✅ Server ready at http://localhost:${PORT}`)
-);
+app.listen(PORT, () => {
+  console.log(`✅ Server ready at http://localhost:${PORT}`);
+  console.log(`📚 Swagger docs at http://localhost:${PORT}/docs`);
+});
