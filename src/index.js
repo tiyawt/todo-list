85ab1b6648ce14239bc2062be478924b72371d10
tiyawt <tiyaland3@gmail.com>
2025-10-15 08:22:47 +0700

    Add CORS support for Swagger

--- src/index.js
+++ src/index.js
@@ -5,11 +5,11 @@ import yaml from "yaml";
 import swaggerUi from "swagger-ui-express";
 import path from "path";
 import { fileURLToPath } from "url";
+import cors from "cors";
 
 import { connectDB } from "./config/db.js";
 import authRoutes from "./routes/auth.routes.js";
 import todoRoutes from "./routes/todo.routes.js";
-
 
 dotenv.config();
 
@@ -19,8 +19,19 @@ const __dirname = path.dirname(__filename);
 const specPath = path.join(__dirname, "../openapi.yaml");
 const spec = yaml.parse(fs.readFileSync(specPath, "utf8"));
 
-
 const app = express();
+
+app.use(
+  cors({
+    origin: "*",
+    methods: ["GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+    credentials: true,
+  })
+);
+
+app.options("*", cors());
+
 app.use(express.json());
 app.use("/docs", swaggerUi.serve, swaggerUi.setup(spec, { explorer: true }));
 
